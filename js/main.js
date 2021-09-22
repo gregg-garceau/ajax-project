c47c9a10b0373a2f2d71c23cd858a16d915d61f0
Gregg Garceau <ggarceau212@gmail.com>
2021-09-22 02:35:18 +0000

    User can start to view pokemon names on right side

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var $pokeName = document.querySelector('.poke-name');
+var $pokeId = document.querySelector('.poke-id');
+var $pokeImage = document.querySelector('.poke-image');
+var $pokeWeight = document.querySelector('.poke-weight');
+var $pokeHeight = document.querySelector('.poke-height');
+var $pokeTypeOne = document.querySelector('.poke-type-one');
+var $pokeTypeTwo = document.querySelector('.poke-type-two');
+var $pokeList = document.querySelectorAll('.list-item');
+var $prevButton = document.querySelector('.prev-btn');
+var $nextButton = document.querySelector('.next-btn');
+var prevUrl = null;
+var nextUrl = null;
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.substr(1);
+}
+
+function pokeList(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    var pokeResults = xhr.response.results;
+    prevUrl = xhr.response.previous;
+    nextUrl = xhr.response.next;
+
+    for (let i = 0; i < $pokeList.length; i++) {
+      var currentPokeList = $pokeList[i];
+      var pokeData = pokeResults[i];
+
+      if (pokeData) {
+        var pokeName = pokeData.name;
+        var pokeUrl = pokeData.url;
+        var splitUrl = pokeUrl.split('/');
+        var pokeId = splitUrl[splitUrl.length - 2];
+        currentPokeList.textContent = pokeId + '. ' + capitalize(pokeName);
+      } else {
+        currentPokeList.textContent = '';
+      }
+    }
+  });
+  xhr.send();
+}
+
+pokeList('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
